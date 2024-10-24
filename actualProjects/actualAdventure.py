70df92deb80bfb941c8122b1f089b2e126f7af02
PBI\OM543ND <omar.ndao1@pb.com>
2024-10-24 15:33:15 -0400

    pushing the actualAdventure for Now

---
+++ actualProjects/actualAdventure.py
@@ -0,0 +1,12 @@
+def start():
+    print("Welcome to Omars true Adventure game:")
+    global start = print("Your name is: " + input())
+    print(start)
+start()
+
+def wakeup():
+    global start()
+    print("You wake up in the middle of nowhere,\n with nothing left to your name after breaking up with your girlfriend, leaving your job and having nothing to your name except for your bills and creativity and ambition, what will you do next?")
+    print(start)
+wakeup()
+    
