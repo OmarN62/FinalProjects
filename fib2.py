6def721689c1c418fb8bf140f65a28bae58a9b38
PBI\OM543ND <omar.ndao1@pb.com>
2024-10-24 15:01:52 -0400

    pushing the rest of the turtle projects

--- fib2.py
+++ fib2.py
@@ -1,3 +1,9 @@
+#define a function named fib that works on the fibonacci
+#function that takes a variable n 
+# and for variable a and b
+#while the variable a is less than n (n = input)
+#print a to n's input
+#a
 def fib(n):
     a, b = 0, 1
     while a < n:
