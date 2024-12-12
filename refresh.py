9e1558814e82214c03dfa0539aa4ca7b1351f7a9
OmarN62 <ndao1233@gmail.com>
2024-12-12 18:15:30 -0500

    Pushing chapter 3 collatz project

--- refresh.py
+++ refresh.py
@@ -1,4 +1,11 @@
 def func():
-    x = 1
-    print (type(x))
+    for x in range(1,11):
+        print(x)
 func()
+
+def func2():
+    y = 1
+    while(y<=10):
+        print(y) 
+        y+=1
+func2()
