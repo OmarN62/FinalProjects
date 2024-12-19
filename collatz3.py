d86c41c87dc8feb008cbafba7682abd5ba79aff2
OmarN62 <ndao1233@gmail.com>
2024-12-19 16:40:18 -0500

    Beginning to work on chapter 4

---
+++ collatz3.py
@@ -0,0 +1,10 @@
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        return result 
+n = input("What's your number?:")
+while n != 1:
+    n = collatz(int(n))
